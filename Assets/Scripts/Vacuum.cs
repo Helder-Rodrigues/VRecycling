9f580be6478452139467553a8a4804d696ebb48b
Hélder <hmrodrigues4@gmail.com>
2026-03-24 12:54:16 +0000

    progress bar

--- Assets/Scripts/Vacuum.cs
+++ Assets/Scripts/Vacuum.cs
@@ -1,12 +1,23 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Vacuum : MonoBehaviour
 {
+    [SerializeField] private TrashGroundSpawner trashInfo;
+    [SerializeField] private Slider slider;
+
+    private float trashValue = 0.1f;
+    
+    private void Start()
+    {
+        trashValue = 1 / trashInfo.trashQtyToSpawn + 0.001f;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Trash"))
+        if (!other.CompareTag("Trash") || slider.value > 0.99f)
             return;
 
         PrefabReference refScript = other.GetComponent<PrefabReference>();
@@ -14,5 +25,6 @@ public class Vacuum : MonoBehaviour
             TrashManager.Instance.AddTrashToList(refScript.originalPrefab);
 
         Destroy(other.gameObject);
+        slider.value += trashValue;
     }
 }
