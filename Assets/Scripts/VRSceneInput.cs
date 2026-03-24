f2aa32d0c2e89586e83324740f856ff8b01d661b
Hélder <hmrodrigues4@gmail.com>
2026-03-24 15:15:47 +0000

    fade-in/out & Change Scene

---
+++ Assets/Scripts/VRSceneInput.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class VRSceneInput : MonoBehaviour
+{
+    [SerializeField] private SceneFader fader;
+    [SerializeField] private string sceneToLoad;
+
+    void Update()
+    {
+        if (
+            OVRInput.GetDown(OVRInput.Button.One) ||   
+            OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            fader.FadeAndLoadScene(sceneToLoad);
+        }
+    }
+}
