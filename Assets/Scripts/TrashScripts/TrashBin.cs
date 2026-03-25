b2828bddb768c17f0dc036ba5b75ce41d4f9bb3d
kobs74 <e12314654@student.tuwien.ac.at>
2026-03-25 10:13:29 +0100

    Added Soundeffects
    feat: added soundseffects to the objects, vacuum and the trash bins

--- Assets/Scripts/TrashScripts/TrashBin.cs
+++ Assets/Scripts/TrashScripts/TrashBin.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class TrashBin : MonoBehaviour
 {
     [SerializeField] private TrashType trashType;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip winningSound;
+    [SerializeField] private AudioClip failureSound;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,11 +15,13 @@ public class TrashBin : MonoBehaviour
             if (trashType == trash.TrashType) // Correct trash in bin
             {
                 print("Correct trash type");
+                audioSource.PlayOneShot(winningSound);
                 TreeManager.Instance.GrowTree();
             }
             else // Wrong trash
             {
                 print("Wrong trash type");
+                audioSource.PlayOneShot(failureSound);
                 TreeManager.Instance.ShrinkTree();
                 
             }
