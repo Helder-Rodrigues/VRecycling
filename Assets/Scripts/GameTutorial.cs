60ad11e6808fdcb148a50dedb14d92d25029a255
Takotzo <oyvindroberg@hotmail.no>
2026-03-27 16:14:57 +0100

    Update GameTutorial.cs

--- Assets/Scripts/GameTutorial.cs
+++ Assets/Scripts/GameTutorial.cs
@@ -1,21 +1,93 @@
+using System;
+using System.Diagnostics;
 using UnityEngine;
 
 public class GameTutorial : MonoBehaviour
 {
-    private enum MyEnum
+    private enum TutorialState
     {
-        
+        Begin,
+        GrabVacuum,
+        VacuumTrash,
+        VacuumFull,
+        GoToTable,
+        PressButton,
+        SortTrash,
+        End
     }
     
     [SerializeField] private GameGuide guide;
     
-    void Start()
+    
+
+    private TutorialState backerState;
+    
+    private TutorialState currentState
     {
-        
+        get => backerState;
+        set
+        {
+            if (backerState == value) return;
+            backerState = value;
+            EnterState();
+        }
     }
 
-    void Update()
+    private void Start()
     {
-        
+        currentState = TutorialState.Begin;
+    }
+
+    void EnterState()
+    {
+        switch (currentState)
+        {
+            case TutorialState.Begin:
+                break;
+            case TutorialState.GrabVacuum:
+                break;
+            case TutorialState.VacuumTrash:
+                break;
+            case TutorialState.VacuumFull:
+                break;
+            case TutorialState.GoToTable:
+                break;
+            case TutorialState.PressButton:
+                break;
+            case TutorialState.SortTrash:
+                break;
+            case TutorialState.End:
+                break;
+
+        }
+    }
+
+    private void Update()
+    {
+        CheckState();
+    }
+
+    void CheckState()
+    {
+        switch (currentState)
+        {
+            case TutorialState.Begin:
+                break;
+            case TutorialState.GrabVacuum:
+                break;
+            case TutorialState.VacuumTrash:
+                break;
+            case TutorialState.VacuumFull:
+                break;
+            case TutorialState.GoToTable:
+                break;
+            case TutorialState.PressButton:
+                break;
+            case TutorialState.SortTrash:
+                break;
+            case TutorialState.End:
+                break;
+
+        }
     }
 }
